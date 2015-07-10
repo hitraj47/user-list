f20a3094ae81ce96792dc91788ce97f752a2e2a7
Raj Ramsaroop <raj@bewareofraj.com>
2015-07-10 13:01:13 -0400

    add json adapter

--- app/src/main/java/com/bewareofraj/userlist/users/User.java
+++ app/src/main/java/com/bewareofraj/userlist/users/User.java
@@ -1,7 +1,13 @@
 package com.bewareofraj.userlist.users;
 
 /**
+ * This class represents a User
  * Created by raj on 7/10/15.
  */
 public class User {
+
+    private int id;
+    private String username;
+    private String email;
+
 }
---
+++ app/src/main/java/com/bewareofraj/userlist/util/JsonAdapter.java
@@ -0,0 +1,68 @@
+package com.bewareofraj.userlist.util;
+
+import android.app.Activity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListAdapter;
+import android.widget.TextView;
+
+import com.bewareofraj.userlist.R;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+/**
+ * Created by raj on 7/10/15.
+ */
+public class JsonAdapter extends BaseAdapter implements ListAdapter {
+
+    private Activity activity;
+    private JSONArray array;
+
+    public JsonAdapter(Activity activity, JSONArray array) {
+        this.activity = activity;
+        this.array = array;
+    }
+
+    @Override
+    public int getCount() {
+        if (array == null) {
+            return 0;
+        } else {
+            return array.length();
+        }
+    }
+
+    @Override
+    public JSONObject getItem(int position) {
+        if (array == null) {
+            return null;
+        } else {
+            return array.optJSONObject(position);
+        }
+    }
+
+    @Override
+    public long getItemId(int position) {
+        JSONObject object = getItem(position);
+
+        return object.optLong("id");
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            convertView = activity.getLayoutInflater().inflate(R.layout.fragment_user_list_item, null);
+        }
+
+        TextView txtUserName = (TextView) convertView.findViewById(R.id.user_name);
+        JSONObject object = getItem(position);
+        if (object != null) {
+            String name = object.optString("name");
+            txtUserName.setText(name);
+        }
+
+        return convertView;
+    }
+}
