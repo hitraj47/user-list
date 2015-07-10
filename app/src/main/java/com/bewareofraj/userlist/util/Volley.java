4beea20a8c744b1c5850b574a3dfc5b377320cef
Raj Ramsaroop <raj@bewareofraj.com>
2015-07-10 08:54:31 -0400

    add volley singleton

---
+++ app/src/main/java/com/bewareofraj/userlist/util/Volley.java
@@ -0,0 +1,13 @@
+package com.bewareofraj.userlist.util;
+
+/**
+ * Created by raj on 7/10/15.
+ */
+public class Volley {
+
+    private static Volley instance;
+
+    public static Volley getInstance() {
+        return instance;
+    }
+}
