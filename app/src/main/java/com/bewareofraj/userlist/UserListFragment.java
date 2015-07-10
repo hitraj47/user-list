e1e763e96df15c91ed675d2ec254348527a499b8
Raj Ramsaroop <raj@bewareofraj.com>
2015-07-10 12:38:15 -0400

    create users package

--- app/src/main/java/com/bewareofraj/userlist/UserListFragment.java
+++ app/src/main/java/com/bewareofraj/userlist/UserListFragment.java
@@ -6,6 +6,8 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 
@@ -13,6 +15,11 @@ import android.widget.TextView;
  * A simple {@link Fragment} subclass.
  */
 public class UserListFragment extends Fragment {
+
+    // test string array
+    private String[] items = {"User 1", "User 2", "User 3", "User 4", "User 5"};
+
+    private ArrayAdapter<String> adapter;
 
 
     public UserListFragment() {
@@ -23,7 +30,12 @@ public class UserListFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_user_list, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_user_list, container, false);
+
+        adapter = new ArrayAdapter<String>(getActivity(), R.layout.fragment_user_list, items);
+        ListView listView = (ListView) rootView.findViewById(R.id.user_list);
+
+        return rootView;
     }
 
 
---
+++ app/src/main/java/com/bewareofraj/userlist/users/User.java
@@ -0,0 +1,7 @@
+package com.bewareofraj.userlist.users;
+
+/**
+ * Created by raj on 7/10/15.
+ */
+public class User {
+}
