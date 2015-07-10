807ab278e20deb44961941a4e28f25a31eda5cef
Raj Ramsaroop <raj@bewareofraj.com>
2015-07-10 09:41:36 -0400

    replace settings with refresh menu item

--- app/src/main/java/com/bewareofraj/userlist/MainActivity.java
+++ app/src/main/java/com/bewareofraj/userlist/MainActivity.java
@@ -41,7 +41,7 @@ public class MainActivity extends ActionBarActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_refresh) {
             return true;
         }
 
